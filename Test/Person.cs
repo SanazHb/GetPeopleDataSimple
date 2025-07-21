95f4d71c09efb92840a6a41e983e51de2f8cabeb
Sanaz Habibollahi <sanaz.habibollahi398@gmail.com>
2025-07-21 10:55:12 +0330

    Add project files.

---
+++ Test/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Person
+    {
+        public string name;
+        public string family;
+        public int age;
+
+        public Person(string name, string family, int age)
+        {
+            this.name = name;
+            this.family = family;
+            this.age = age;
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //تعداد افراد
+            Console.WriteLine("Enter the number of people:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //تعریف آرایه بر اساس تعداد افراد
+            Person[] People = new Person[number];
+            for(int i = 0; i < number; i++)
+            {
+                //گرفتن اسم
+                Console.WriteLine($"Enter person [{i + 1}] name: ");
+                string name = Console.ReadLine();
+                //گرفتن فامیلی
+                Console.WriteLine($"Enter person [{i + 1}] family: ");
+                string family = Console.ReadLine();
+                //گرفتن سن
+                Console.WriteLine($"Enter person [{i + 1}] age: ");
+                int age =  Convert.ToInt32(Console.ReadLine());
+                //نمونه سازی
+                Person p = new Person(name, family, age);
+                //نگهداری در آرایه
+                People[i] = p;
+            }
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            foreach (Person person in People)
+            {
+                Console.WriteLine($"Name: {person.name}, Family: {person.family}, Age: {person.age}");
+            }
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
